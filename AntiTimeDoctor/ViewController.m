80e3b3afb1ed8fb32f913d7da13f691f9292b247
hamdy <dimo.hamdy@gmail.com>
2015-09-26 16:26:13 +0200

    add references
    add references for send keystrokes and make app hidden

--- AntiTimeDoctor/ViewController.m
+++ AntiTimeDoctor/ViewController.m
@@ -13,6 +13,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    // make the app not visable
+    //http://www.raywenderlich.com/98178/os-x-tutorial-menus-popovers-menu-bar-apps
+    
     // Do any additional setup after loading the view.
     
     [NSTimer scheduledTimerWithTimeInterval:1
@@ -47,6 +50,10 @@
     //for more keys check this http://web.archive.org/web/20100501161453/http://www.classicteck.com/rbarticles/mackeyboard.php
     
     
+    
+    // https://developer.apple.com/library/mac/documentation/Carbon/Reference/QuartzEventServicesRef/index.html#//apple_ref/c/func/CGEventCreateKeyboardEvent
+    
+//    http://stackoverflow.com/questions/2666154/cocoa-virtual-keystrokes-pain
     CGEventRef event1, event2, event3, event4;
     event1 = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)126, true);
     event2 = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)126, false);
