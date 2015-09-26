2fce496a1fc90969906900714020dea97a7b9658
hamdy <dimo.hamdy@gmail.com>
2015-09-26 17:26:37 +0200

    add shortcut
    add DDHotKey framework to handle shortcut

--- AntiTimeDoctor/AppDelegate.m
+++ AntiTimeDoctor/AppDelegate.m
@@ -7,7 +7,8 @@
 //
 
 #import "AppDelegate.h"
-
+#import "DDHotKeyCenter.h"
+#import <Carbon/Carbon.h>
 @interface AppDelegate ()
 
 @end
@@ -16,6 +17,35 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    DDHotKeyCenter *c = [DDHotKeyCenter sharedHotKeyCenter];
+    
+    //To close App Command+V
+    
+    if (![c registerHotKeyWithKeyCode:kVK_ANSI_V modifierFlags:NSCommandKeyMask target:self action:@selector(hotkeyWithEvent:) object:nil]) {
+        [self addOutput:@"Unable to register hotkey for example 1"];
+    } else {
+        [self addOutput:@"Registered hotkey for example 1"];
+        [self addOutput:[NSString stringWithFormat:@"Registered: %@", [c registeredHotKeys]]];
+    }
+
+}
+- (void) addOutput:(NSString *)newOutput {
+    
+    NSLog(@"%@",newOutput);
+}
+
+- (void) hotkeyWithEvent:(NSEvent *)hkEvent {
+    [self addOutput:[NSString stringWithFormat:@"Firing -[%@ %@]", NSStringFromClass([self class]), NSStringFromSelector(_cmd)]];
+    [self addOutput:[NSString stringWithFormat:@"Hotkey event: %@", hkEvent]];
+    
+    exit(0);
+
+}
+
+- (void) hotkeyWithEvent:(NSEvent *)hkEvent object:(id)anObject {
+    [self addOutput:[NSString stringWithFormat:@"Firing -[%@ %@]", NSStringFromClass([self class]), NSStringFromSelector(_cmd)]];
+    [self addOutput:[NSString stringWithFormat:@"Hotkey event: %@", hkEvent]];
+    [self addOutput:[NSString stringWithFormat:@"Object: %@", anObject]];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
